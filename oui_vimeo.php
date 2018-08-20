ed65014b7ac35e65c7c0253015d77606c72edabc
Nicolas Morand <contact@nicolasmorand.com>
2018-08-20 14:40:32 +0200

    Updates the Provider class namespace

--- oui_vimeo.php
+++ oui_vimeo.php
@@ -31,9 +31,9 @@
 
 namespace Oui;
 
-if (class_exists('Oui\Provider')) {
+if (class_exists('Oui\Player\Provider')) {
 
-    class Vimeo extends Provider
+    class Vimeo extends Player\Provider
     {
         protected static $srcBase = '//player.vimeo.com/';
         protected static $srcGlue = array('video/', '?', '&amp;');
